55da9ab3b9e349f002a10b0b9e01d59dc382b839
Uzair Islam <uzair.islam@A006-00934.local>
2024-03-04 20:09:12 +0500

    Refactored Crawler

--- foodpanda_crawler.py
+++ foodpanda_crawler.py
@@ -8,25 +8,18 @@ from foodpanda_parser import FoodPandaParser
 class FoodPandaCrawlSpider(Spider):
     name = "foodpanda-crawl"
     start_urls = ["https://www.foodpanda.pk/city/lahore"]
-    parse_spider = FoodPandaParser()
-    
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML," \
-            " like Gecko) Chrome/121.0.0.0 Safari/537.36",
-    }
-    
-    def start_requests(self):
-        return [Request(self.start_urls[0], headers=self.headers)]
+    parser = FoodPandaParser()
     
     def parse(self, response):
         css = "script:contains(ItemList)::text"
         raw_restaurants = json.loads(response.css(css).get())["itemListElement"]
         raw_restaurants = [restaurant['item']['url'] for restaurant in raw_restaurants]
 
-        yield from [response.follow(url, self.parse_reviews, headers=self.headers) for url in raw_restaurants]
+        yield from [response.follow(url, self.parser.parse) for url in raw_restaurants]
+        yield from self.parse_pagination(response)
         
-    def parse_reviews(self, response):
-        yield from self.parse_spider.parse(response)
+    def parse_pagination(self, response):
+        return []
 
 
 if __name__ == '__main__':
@@ -40,7 +33,9 @@ if __name__ == '__main__':
         "DOWNLOAD_DELAY": 10,
         "CONCURRENT_REQUESTS": 1,
         "HTTPERROR_ALLOWED_CODES": [403],
-        "ITEM_PIPELINES": pipelines
+        "ITEM_PIPELINES": pipelines,
+        "USER_AGENT": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML," \
+                      " like Gecko) Chrome/121.0.0.0 Safari/537.36",
     })
     process.crawl(FoodPandaCrawlSpider)
     process.start()
